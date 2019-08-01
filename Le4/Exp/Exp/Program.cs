a1bec493b856deaf74e7e66b27782fcfc0213a89
jiayu <jiayu653781964@163.com>
2019-08-01 10:18:06 +0800

    添加课程2-3

---
+++ Le4/Exp/Exp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace jiecheng
+{
+    class jiecheng
+    {
+        public int Jc(int num)
+        {
+
+            return num > 0 ? num * Jc(num - 1) : 1;  //如果num>0则返回num *Jc(num - 1)，否则返回1
+        }
+    }
+
+    class excutejiecheng
+
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                jiecheng n = new jiecheng();
+                int result = n.Jc(Convert.ToInt16(Console.ReadLine()));
+                Console.WriteLine("result is {0}", result);
+                //Console.ReadKey();
+            }
+            
+        }
+    }
+}
