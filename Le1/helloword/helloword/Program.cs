a903a433933ff96745c6103a7c8c8d4f644d139a
jiayu <jiayu653781964@163.com>
2019-08-01 09:45:32 +0800

    测试环境

---
+++ Le1/helloword/helloword/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace HelloWorldApplication
+{
+    /* 类名为 HelloWorld */
+    class HelloWorld
+    {
+        /* main函数 */
+        static void Main(string[] args)
+        {
+            /* 我的第一个 C# 程序 */
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am C#");
+
+            Console.ReadKey();
+        }
+    }
+}
