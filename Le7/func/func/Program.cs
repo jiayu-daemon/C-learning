c6aed81662b1adbe615ca0a9328084a158cc668d
jiayu <jiayu653781964@163.com>
2019-08-23 18:25:42 +0800

    添加代码

---
+++ Le7/func/func/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace CalculatorApplication
+{
+    class NumberManipulator
+    {
+        public int FindMax(int num1, int num2)
+        {
+            /* 局部变量声明 */
+            int result;
+
+            if (num1 > num2)
+                result = num1;
+            else
+                result = num2;
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            /* 局部变量定义 */
+            int a = 100;
+            int b = 200;
+            int ret;
+            NumberManipulator n = new NumberManipulator();
+
+            //调用 FindMax 方法
+            ret = n.FindMax(a, b);
+            Console.WriteLine("最大值是： {0}", ret);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Le7/ref/ref/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace CalculatorApplication
+{
+    class NumberManipulator
+    {
+        public void swap(ref int x, ref int y)
+        {
+            int temp;
+
+            temp = x; /* 保存 x 的值 */
+            x = y;    /* 把 y 赋值给 x */
+            y = temp; /* 把 temp 赋值给 y */
+        }
+
+        public void swapval(int x, int y)
+        {
+            int temp;
+
+            temp = x; /* 保存 x 的值 */
+            x = y;    /* 把 y 赋值给 x */
+            y = temp; /* 把 temp 赋值给 y */
+        }
+
+
+        static void Main(string[] args)
+        {
+            NumberManipulator n = new NumberManipulator();
+            /* 局部变量定义 */
+            int a = 100;
+            int b = 200;
+
+            Console.WriteLine("在交换之前，a 的值： {0}", a);
+            Console.WriteLine("在交换之前，b 的值： {0}", b);
+
+            n.swapval(a,b);
+            Console.WriteLine("1在交换之后，a 的值： {0}", a);
+            Console.WriteLine("1在交换之后，b 的值： {0}", b);
+
+            /* 调用函数来交换值 */
+            n.swap(ref a, ref b);
+
+            Console.WriteLine("2在交换之后，a 的值： {0}", a);
+            Console.WriteLine("2在交换之后，b 的值： {0}", b);
+
+            Console.ReadLine();
+
+        }
+    }
+}
